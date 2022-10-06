c033143cbbaf3c8220e36b5a9239a35989e8b833
Tamas Lazar <lazartamas97@gmail.com>
2022-10-06 13:07:29 +0200

    Add comments to the code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Creating div's to display information about the calculation
 const displayBottom = document.querySelector('.resultbar-bottom');
 let currentValueHolder = document.createElement('div');
 displayBottom.appendChild(currentValueHolder);
@@ -9,7 +10,7 @@ displayTop.appendChild(prevValueHolder);
 let mathResult = '';
 let storedNumber = '';
 let firstNumber = '';
-// currentValueHolder.textContent = ;
+// looping through the buttons that store integer values with a forEach method, to store their values in a variable when the user clicks on them
 const numbers = document.querySelectorAll(".numbers");
 numbers.forEach(number => {
     number.addEventListener('click', () => {
@@ -18,6 +19,7 @@ numbers.forEach(number => {
     })
 });
 
+// doing the same loop as above, for the buttons that store the operator values
 let clickedOperator = '';
 const operators = document.querySelectorAll(".operators");
 operators.forEach(operator => {
@@ -33,6 +35,7 @@ operators.forEach(operator => {
     });
 });
 
+// functions for the basic calculations that calculator can do
 function add(a,b) {
     return a + b;
 }
@@ -46,19 +49,15 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
+    // check if the user wants to use 0 as the denominator
     if (b === 0 || isNaN(b)) {
-        return 'ERROR';
+        return "C'mon mate! No zero";
     } else {
         return a / b;
     }
 }
 
-function divideZero(a, b) {
-    if (result == Infinity) {
-        return currentValueHolder.textContent = 'ERROR';
-    } 
-}
-
+// a function that calls the above functions based on which one of them was clicked
 function operate(num1, num2, operator) {
     switch(operator) {
         case "+":
@@ -72,6 +71,7 @@ function operate(num1, num2, operator) {
     }
 }
 
+// function that calls the operate() function from above and set the value of variable mathResult, it also resets one of the display divs and the variable firstNumber
 const displayResult = () => {
     mathResult = operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator);
     currentValueHolder.textContent = mathResult;
@@ -80,12 +80,13 @@ const displayResult = () => {
     firstNumber = '';
 };
 
+// an eventListener added to the 'equal button' to call the above function
 const btnEqual = document.querySelector('.equal');
 btnEqual.addEventListener('click', e => {
     displayResult();
-    divideZero();
 });
 
+// function to clear everything on the display and to reset every variable
 const clearAll = () => {
     storedNumber = '';
     firstNumber = '';
@@ -94,11 +95,13 @@ const clearAll = () => {
     mathResult = '';
 };
 
+// eventListener to call the above function
 const btnClear = document.querySelector('.allclear');
 btnClear.addEventListener('click', e => {
     clearAll();
 });
 
+// function to delete the last item of the string that displays the current value every time its clicked
 const clearLast = () => {
     storedNumber = storedNumber.slice(0, -1);
     currentValueHolder.textContent = storedNumber;
@@ -106,22 +109,20 @@ const clearLast = () => {
     console.log(typeof(storedNumber));
 };
 
+// eventListener to call the above function
 const btnDelete = document.querySelector('.delete');
 btnDelete.addEventListener('click', e => {
     clearLast();
 });
 
-
+// function that calculates the percentage value ofa number i.e 50 is equal to 0.5
 const makePercentage = () => {
     storedNumber = storedNumber / 100;
     currentValueHolder.textContent = storedNumber;
 };
 
+// eventListener to call the above function
 const btnPercent = document.querySelector('.percent');
 btnPercent.addEventListener('click', e => {
     makePercentage();
 });
-
-// btnDelete.addEventListener('click', e => {
-//     btnDelete.style = 'background-color:hsla(45, 75%, 50%, 0.1)';
-// });
